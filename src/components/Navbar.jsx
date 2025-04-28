c1f3579bb64fa1e70f984a07270a7ff641338a17
Ali <youseflinares@gmail.com>
2025-04-28 01:24:41 -0400

    fix links on nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ export default function Navigation(){
     <nav className="navbar">
       <div className="navbar-brand">
         <img src={brandLogo} alt="React Logo" className="navbar-brand-logo" width="40" height="40"/>
-        <a className="navbar-brand-name nav-link" href="https://github.com/linares-dev/react-facts-deploy.git">ReactFacts</a>
+        <span className="navbar-brand-name nav-link">ReactFacts</span>
       </div>
-      <p><i className="fa-brands fa-github navbar-social-icon"></i></p>
+      <a className="nav-link" href="https://github.com/linares-dev/react-facts-deploy.git"><i className="fa-brands fa-github navbar-social-icon"></i></a>
     </nav>
   )
 }
