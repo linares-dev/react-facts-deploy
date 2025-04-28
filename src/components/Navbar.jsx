bcaa062864118cad30e705f6469b9bc9e3993844
Ali <youseflinares@gmail.com>
2025-04-28 01:17:55 -0400

    add repo url to package.json and update nav link to repo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navigation(){
     <nav className="navbar">
       <div className="navbar-brand">
         <img src={brandLogo} alt="React Logo" className="navbar-brand-logo" width="40" height="40"/>
-        <a className="navbar-brand-name nav-link" href="#">ReactFacts</a>
+        <a className="navbar-brand-name nav-link" href="https://github.com/linares-dev/react-facts-deploy.git">ReactFacts</a>
       </div>
       <p><i className="fa-brands fa-github navbar-social-icon"></i></p>
     </nav>
